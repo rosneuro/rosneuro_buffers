acaaa761b1fe72591211fd59c0ad9df170a3608e
Luca Tonin <luca.tonin@gmail.com>
2023-03-20 17:59:57 +0100

    Refactory

--- src/RingBuffer.cpp
+++
@@ -1,6 +0,0 @@
-#include "rosneuro_buffers/RingBuffer.hpp"
-#include "pluginlib/class_list_macros.h"
-
-PLUGINLIB_EXPORT_CLASS(rosneuro::RingBuffer<int>,    rosneuro::Buffer<int>)
-PLUGINLIB_EXPORT_CLASS(rosneuro::RingBuffer<float>,  rosneuro::Buffer<float>)
-PLUGINLIB_EXPORT_CLASS(rosneuro::RingBuffer<double>, rosneuro::Buffer<double>)
